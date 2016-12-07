9ea1eb0e8bde1ddb3d4f81fe20f5ca8f33282b3e
Tobias Lenz <tobias.lenz1@gmx.de>
2016-12-07 14:56:54 +0100

    Updated Docu. Made it nuget friendly, now aviable as nuget.

--- FastSockets/Properties/AssemblyInfo.cs
+++ FastSockets/Properties/AssemblyInfo.cs
@@ -10,11 +10,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FastSockets")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("FastSocketsAPI")]
+[assembly: AssemblyDescription("C# .NET 3.5 Multithreaded Network Library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FastSockets")]
+[assembly: AssemblyCompany("Tobias Lenz")]
+[assembly: AssemblyProduct("FastSocketsAPI")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -37,5 +37,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
